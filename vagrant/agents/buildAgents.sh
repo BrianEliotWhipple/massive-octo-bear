20c262cfb5480c5b61c13b5d01665943b27d3b66
bwhipple <bwhipple@pros.com>
2015-07-02 12:46:45 -0500

    add docker agent publishing to registry

--- vagrant/agents/buildAgents.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -t example/gocd-agent-java-8:0.1 -f Dockerfile.gocd-agent-java-8 .
-
---
+++ vagrant/agents/buildAndPublishAgents.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$1" != "" ]; then
+    docker_registry_url="$1"
+    echo "Publishing agents into docker registry: ${docker_registry_url}"
+else
+    echo "Docker registry url is required."
+    exit 1
+fi
+
+docker build -t example/gocd-agent-java-8:0.1 -f Dockerfile.gocd-agent-java-8 .
+
+docker tag example/gocd-agent-java-8:0.1 ${docker_registry_url}/example/gocd-agent-java
+-8:0.1
+
+docker push ${docker_registry_url}/example/gocd-agent-java
+-8:0.1
+
+docker rmi example/gocd-agent-java-8:0.1
