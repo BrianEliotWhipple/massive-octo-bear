b07cd5db1adb7e81f7d1d8c65e5326868e62cb07
bwhipple <bwhipple@pros.com>
2015-07-02 15:25:27 -0500

    Fixed minor errors

--- vagrant/agents/buildAndPublishAgents.sh
+++ vagrant/agents/buildAndPublishAgents.sh
@@ -10,8 +10,9 @@ fi
 
 docker build -t example/gocd-agent-java-8:0.1 -f Dockerfile.gocd-agent-java-8 .
 
-docker tag example/gocd-agent-java-8:0.1 ${docker_registry_url}/example/gocd-agent-java-8:0.1
+docker tag -f example/gocd-agent-java-8:0.1 ${docker_registry_url}/example/gocd-agent-java-8:0.1
 
 docker push ${docker_registry_url}/example/gocd-agent-java-8:0.1
 
 docker rmi example/gocd-agent-java-8:0.1
+docker rmi ${docker_registry_url}/example/gocd-agent-java-8:0.1
