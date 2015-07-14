f0094ede8d346dcdb15b03a6b408710ef31552b0
bwhipple <brian.eliot.whipple@gmail.com>
2015-07-14 15:16:55 -0500

    Improve deis workstation config; reducing service to one port

--- vagrant/deis/install_deis_platform.sh
+++ vagrant/deis/install_deis_platform.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-eval `ssh-agent -s`
-ssh-add ~/insecure_private_key
+deisctl config platform set sshPrivateKey=~/insecure_private_key
+deisctl config platform set domain=local3.deisapp.com
+deisctl refresh-units
+deisctl install platform
+deisctl start platform
 
-deisctl --tunnel=local3.deisapp.com config platform set sshPrivateKey=~/insecure_private_key
-deisctl --tunnel=local3.deisapp.com config platform set domain=local3.deisapp.com
-deisctl --tunnel=local3.deisapp.com refresh-units
-deisctl --tunnel=local3.deisapp.com install platform
-deisctl --tunnel=local3.deisapp.com start platform
--- vagrant/deis/install_deis_workstation.sh
+++ vagrant/deis/install_deis_workstation.sh
@@ -3,7 +3,7 @@
 curl -sSL http://deis.io/deis-cli/install.sh | sh
 sudo ln -fs $PWD/deis /usr/local/bin/deis
 
-curl -sSL http://deis.io/deisctl/install.sh | sudo sh -s 1.7.3
+curl -sSL http://deis.io/deisctl/install.sh | sudo sh -s 1.8.0
 sudo ln -fs $PWD/deisctl /usr/local/bin/deisctl
 
 cp /vagrant/insecure_private_key /home/vagrant/insecure_private_key
@@ -11,4 +11,12 @@ chown vagrant /home/vagrant/insecure_private_key
 chgrp vagrant /home/vagrant/insecure_private_key
 chmod 0600 /home/vagrant/insecure_private_key
 
+cp /vagrant/deis/set_deis_profile.sh /home/vagrant/set_deis_profile.sh
+chown vagrant /home/vagrant/set_deis_profile.sh
+chgrp vagrant /home/vagrant/set_deis_profile.sh
+chmod 0755 /home/vagrant/set_deis_profile.sh
+
+echo "
+source /home/vagrant/set_deis_profile.sh
+" >> /home/vagrant/.bash_profile
 
