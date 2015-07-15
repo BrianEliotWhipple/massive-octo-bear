eb86ebcb5663cf840f29d9f8165a0b07055a40f7
bwhipple <brian.eliot.whipple@gmail.com>
2015-07-15 08:36:02 -0500

    Added log msg to marathon scripts

--- mesos/create_application_group.sh
+++ mesos/create_application_group.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 marathon_host="172.17.8.11"
-echo "Creating apps on marathon host: ${marathon_host}"
 
+echo "Deleting example application group on marathon host: ${marathon_host}"
 curl -X DELETE http://${marathon_host}:8080/v2/groups/example
 
+echo "Creating example application group on marathon host: ${marathon_host}"
 curl -X POST -H "Content-Type: application/json" --data @example_app_group.json http://${marathon_host}:8080/v2/groups
--- mesos/create_apps.sh
+++ mesos/create_apps.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 marathon_host="172.17.8.11"
-echo "Creating apps on marathon host: ${marathon_host}"
 
+echo "Creating cassandra application on marathon host: ${marathon_host}"
 curl -X POST -H "Content-Type: application/json" --data @cassandra_application.json http://${marathon_host}:8080/v2/apps
 
+echo "Creating echo service application on marathon host: ${marathon_host}"
 curl -X POST -H "Content-Type: application/json" --data @echo_application.json http://${marathon_host}:8080/v2/apps
