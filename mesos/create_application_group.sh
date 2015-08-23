d542ef8f7f3da6bda712b6e0d415cb2816140c03
bwhipple <brian.eliot.whipple@gmail.com>
2015-08-23 18:39:12 -0500

    Fixing deployment lock problem

--- mesos/create_application_group.sh
+++ mesos/create_application_group.sh
@@ -5,5 +5,8 @@ marathon_host="172.17.8.10"
 echo "Deleting example application group on marathon host: ${marathon_host}"
 curl -X DELETE http://${marathon_host}:8080/v2/groups/example
 
+echo "Sleeping 30s..."
+sleep 30s
+
 echo "Creating example application group on marathon host: ${marathon_host}"
 curl -X POST -H "Content-Type: application/json" --data @example_app_group.json http://${marathon_host}:8080/v2/groups
